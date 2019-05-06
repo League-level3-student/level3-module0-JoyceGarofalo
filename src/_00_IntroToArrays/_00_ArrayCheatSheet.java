5097aeb6dbf8d55cbc05122151a67d6e2062b7e0
Joyce Garofalo <garofalo.je@gmail.com>
2019-05-05 17:28:33 -0700

    Intro to arrays

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -1,24 +1,28 @@
 package _00_IntroToArrays;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-
+		String[] strings = new String[5];
 		//2. print the third element in the array
-
+		System.out.println(strings[2]);
 		//3. set the third element to a different value
-
+		strings[2] = "string 2";
 		//4. print the third element again
-		
+		System.out.println(strings[2]);
 		//5. use a for loop to set all the elements in the array to a string of your choice
-		
+		for (int i = 0; i < strings.length; i++) {
+			strings[i] = "string " + i;
+			System.out.println(i);
+		}
 		//6. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
 		
 		//7. make an array of 50 integers
-
+		
 		//8. use a for loop to make every value of the integer array a random number
 
 		//9. without printing the entire array, print only the smallest number on the array
