57e9a5dfd41ad548efcce8dc34fd3ad1b2a19165
league <league@wts14-4.attlocal.net>
2019-06-02 17:26:01 -0700

    Robot Race, Find the hidden button, visual array sorter ip

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -2,6 +2,7 @@ package _00_IntroToArrays;
 
 import java.util.Random;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
@@ -9,6 +10,7 @@ import org.jointheleague.graphical.robot.Robot;
 public class _01_RobotRace {
 	//1. make a main method
 	public static void main(String[] args) {
+		
 		//2. create an array of 5 robots.
 		Robot[] robot = new Robot[5];
 		Random rand = new Random();
@@ -21,24 +23,26 @@ public class _01_RobotRace {
 			robot[i].setY(550);
 			robot[i].setSpeed(30);
 		}
+		//5. use another for loop to iterate through the array and make each robot move 
+		   //   a random amount less than 50.
+			    	
+		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 		while (isRacing == true) {
 			for (int j = 0; j < robot.length; j++) {
 				int r = rand.nextInt(50);
 				robot[j].move(r);
-				if(robot[j].getY()> 0) {
+				if(robot[j].getY()< 0) {
 					isRacing = false;
 					System.out.println("winner!");
 					break;
+					
 				}
 			}
+			
 		}
 		
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	   //   a random amount less than 50.
-		    	
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
+	
 	//7. declare that robot the winner and throw it a party!
 		    	
 	//8. try different races with different amounts of robots.
